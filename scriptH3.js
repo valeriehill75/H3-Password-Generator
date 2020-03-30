569a6f05002d5ec072b15522f0ad8d5530c1e23f
Valerie Hill <itsvhill@gmail.com>
2020-03-29 22:49:56 -0500

    attempts at all other requirements each of which failed miserably.

--- scriptH3.js
+++ scriptH3.js
@@ -1,23 +1,38 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+///var generateBtn = document.querySelector("#generate");
 event.preventDefault ()
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword)
+///generateBtn.addEventListener("click", writePassword)
 
 function submitButton () {
-//designate min, max, and special character parameters
-var minCharacter = prompt("Enter the minimum number of characters in your password.\n(Must be at least 8)")
-var maxCharacter = prompt("Enter the maximum number of characters in your password.\n(Must be fewer than 128)")
-var specCharacter = prompt("Include special characters? (Y/N)")
+//designate number of characters, and special character parameters
+  var passCharacter = prompt("Enter the number of characters in your password.\n(Must be at least 8 and fewer than 128)");
+    if (passCharacter < 8 || passCharacter === null) {
+      alert ("Please enter the correct response.");
+    }
+
+  var specCharacter = prompt("Include special characters? Uppercase? Lowercase? Numbers? Special Characters?\n(Enter at least one of the above)");
+    if (specCharacter === null || specCharacter !== Y || specCharacter !== N) {
+      alert ("Please enter the correct response."); 
+    }
 }
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//function writePassword() {
+//  var password = generatePassword(); {
+//    let passLength = document.getElementById("passCharacter").value
 
-  passwordText.value = password;
+//    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890`~!@#$%^&*()-_=+[]\{}|;':",./<>?";
+
+//    let passLength = "";
+
+//    for (var i = 0, i <= passLength; i++)
+//      password = password + values.charAt(Math.floor(Math.random() * Math.floor(value.length -1)));
+//  }
+//  var passwordText = document.querySelector("#password");
+
+//  passwordText.value = password;
 
 }
 
